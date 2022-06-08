e0ea833b33f733bea3ab472bcd9ef1a54d389b07
kanigreg <kanigreg@gmail.com>
2022-06-08 20:48:20 +0500

    Solve arrays chellenge

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,38 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)
-        array
+      def max(array)
+        return nil if array.empty?
+
+        iter = array.each
+        max = iter.next
+        for i in iter
+          max = i if i > max
+        end
+        max
       end
 
-      def search(_array, _query)
-        0
+      def replace(array)
+        retrun [] if array.empty?
+
+        max = max(array)
+        array.map{ |item| item > 0 ? max : item }
+      end
+
+      def search(array, query)
+        search_helper = ->(left, right) do
+          return -1 if left > right
+
+          middle = (left+right) / 2
+          if array[middle] == query
+            middle
+          elsif query > array[middle]
+            search_helper.call(middle + 1, right)
+          else
+            search_helper.call(left, middle - 1)
+          end
+        end
+        search_helper.call(0, array.length - 1)
       end
     end
   end
